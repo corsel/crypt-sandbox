55a8ae4a77a83969cc47bf40b8700c40870dff6e
cors <cors@localhost.localdomain>
2016-05-18 20:03:05 +0300

    multiple file input added. passphrase entry is now on app level (instead of shell).

--- main.c
+++ main.c
@@ -5,40 +5,54 @@
 #include <stdio.h>
 
 unsigned char keyHash[SHA256_DIGEST_LENGTH];
-unsigned char buffer[1024];
+unsigned char inBuffer[1024];
+unsigned char outBuffer[1024];
 
-void encryptAes(unsigned char *argData)
+FILE *inFile, *outFile;
+
+int readFile(unsigned char *argFilename)
+{
+	printf("debug - file name: %s\n", argFilename);
+	inFile = fopen(argFilename, "r");
+	if (!inFile)
+	{
+		printf("error - file not found: %s. skipping...\n", argFilename);
+		return 1;
+	}
+	return 0;
+}
+
+void encryptAes()
 {
 	AES_KEY aesKey;
 	AES_set_encrypt_key(keyHash, 256, &aesKey);
-	AES_encrypt(argData, buffer, &aesKey);
+	AES_encrypt(inBuffer, outBuffer, &aesKey);
 	printf("debug - encrypted text: ");
 	int i = 0;
 	for (; i < 1024; i++)
 	{
-		if (keyHash[i] == '\0')
+		if (outBuffer[i] == '\0')
 			break;
-		printf("%02x", keyHash[i]);
+		printf("%02x ", outBuffer[i]);
 	};
 	printf("\n");
 }
 
-void generateHash(unsigned char *argPass)
+void generateHash(unsigned char *argPassphrase)
 {
 	unsigned char key[SHA256_DIGEST_LENGTH];
 	unsigned char *data;
 	
 	data = malloc(10 * sizeof(unsigned char));
 	
-	strcpy(key, argPass);
-	printf("debug - check: %s\n", key);
+	strcpy(key, argPassphrase);
 	
 	SHA256_CTX handle;
 	SHA256_Init(&handle);
 	SHA256_Update(&handle, key, strlen(key));
 	SHA256_Final(keyHash, &handle);
 	
-	printf("debug - sha256 hash: ");
+	printf("debug - passphrase: %s\n\tsha256 hash: ", argPassphrase);
 	int i = 0;
 	for (; i < SHA256_DIGEST_LENGTH; i++)
 	{
@@ -49,21 +63,27 @@ void generateHash(unsigned char *argPass)
 	free(data);
 }
 
-void loadFile(char *argFileName)
-{
-	FILE *handle;
-	handle = fopen(argFileName, "r");
-	fclose(handle);
-}
-
 int main(int argc, char **argv)
 {
-	if (argc == 1)
+	if (argc <= 1)
 	{
-		printf("error - no arguments passed. terminating.\n");
+		printf("error - needs at least 2 arguments.\n");
 		return 1;
 	}
-	generateHash(argv[1]);
-	encryptAes(argv[2]);
+	unsigned char keyBuffer[256];
+	//TODO: check if entering 256+ chars is safe. 
+	//TODO: masked input?
+	printf("Please enter passphrase: ");
+	scanf("%s", keyBuffer);
+	generateHash(keyBuffer);
+	int i = 1; 
+	for (; i < argc; i++)
+	{
+		if (!readFile(argv[i]))
+		{
+			encryptAes();
+			fclose(inFile);
+		}
+	}
 	return 0;
 }
